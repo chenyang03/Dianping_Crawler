fe11239505477c6096f9f926d1d572ce9076b53b
Yuan-T-Xuan <tonyxuan26@foxmail.com>
2016-01-11 02:22:43 +0800

    finish install.sh, tested

--- User_Crawler/dianping_u_script.py
+++ User_Crawler/dianping_u_script.py
@@ -1,20 +1,20 @@
 import dianping_u_crawler_main
 
-begin_ID = 56644299
-end_ID = 60000000
-step = 7000
+begin_ID = 1
+end_ID = 100000
+step = 10000
 
 def Process(N):
     try:
         dianping_u_crawler_main.getInRange(N, end_ID, step)
-        S = open("../status.txt", "w")
+        S = open("./status.txt", "w")
         S.write("finished\n")
         S.close()
     except:
-        file = open("../status.txt")
+        file = open("./status.txt")
         num = int(file.read())
         file.close()
-        Process(num + 14000)
+        Process(num + step)
 
 Process(begin_ID)
 

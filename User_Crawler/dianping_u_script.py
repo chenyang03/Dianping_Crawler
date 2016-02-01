800f74fbdc4a3a1db54b7689ae395af42477242f
Zhuang Zhou <farrellsc@163.com>
2016-02-01 22:16:31 +0800

    Actually we can delete this script.py...
    We can add this initiation command to the end of main.py

--- User_Crawler/dianping_u_script.py
+++ User_Crawler/dianping_u_script.py
@@ -1,20 +1,4 @@
 import dianping_u_crawler_main
 
-begin_ID = 1019703
-end_ID = 1019704
-step = 10000
-
-def Process(N):
-    try:
-        dianping_u_crawler_main.getInRange(N, end_ID, step)
-        S = open("./status.txt", "w")
-        S.write("finished\n")
-        S.close()
-    except:
-        file = open("./status.txt")
-        num = int(file.read())
-        file.close()
-        Process(num + step)
-
-Process(begin_ID)
-
+dianping_u_crawler_main.getInRange('./inputfile.txt')
+#deleted command lines considering 'status.txt'
